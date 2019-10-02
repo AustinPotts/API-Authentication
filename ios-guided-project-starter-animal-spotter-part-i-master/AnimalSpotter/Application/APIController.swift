8ee0448736dc56c0dbafb19b86c59ed8b1f568fe
AustinPotts <pottsevolvements@gmail.com>
2019-10-02 12:59:22 -0400

    Configured Parse Model + SignUp HTTP Method

--- ios-guided-project-starter-animal-spotter-part-i-master/AnimalSpotter/Application/APIController.swift
+++ ios-guided-project-starter-animal-spotter-part-i-master/AnimalSpotter/Application/APIController.swift
@@ -14,11 +14,63 @@ enum HTTPMethod: String {
     case post = "POST"
 }
 
+enum NetworkError: Error{
+    case ecodingError
+    case responseError
+    case otherError(Error)
+    case noData
+    case noDecode
+    case noToken
+}
+
 class APIController {
     
     private let baseUrl = URL(string: "https://lambdaanimalspotter.vapor.cloud/api")!
     
     // create function for sign up
+    func signUp(with user: User, completion: @escaping(NetworkError?)-> Void){
+        
+        //Build the URL
+        let requestURL = baseUrl.appendingPathComponent("users").appendingPathComponent("signup")
+        
+        //Build the request
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        
+        //Turn request into JSON
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        //Perform the request
+        let encoder = JSONEncoder()
+        
+        do {
+           let userJSON = try encoder.encode(user)
+            request.httpBody = userJSON
+        } catch {
+            NSLog("Error encoding data: \(error)")
+            completion(.ecodingError)
+            return
+            
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(.responseError)
+                return
+            }
+            
+            if let error = error {
+                NSLog("Error Creating user on server: \(error)")
+                completion(.otherError(error))
+                return
+            }
+            completion(nil)
+        }.resume()
+        
+    }
+    
     
     // create function for sign in
     
---
+++ ios-guided-project-starter-animal-spotter-part-i-master/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  AnimalSpotter
+//
+//  Created by Austin Potts on 10/2/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+struct User: Codable {
+    let username: String
+    let password: String 
+}
